a5d16c2715d41afd42c579b2b0e6a2c1e56f1aa5
GeancarlosCassagne <geancarloscassagne15@gmail.com>
2026-06-23 09:11:22 -0500

    Solucionar inicializacion de supabase en build

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Si la variable no se encuentra en el build, le pasamos un texto temporal para que no rompa
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://placeholder.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'placeholder-key';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
